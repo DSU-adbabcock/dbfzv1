dbf125f6205df9f4a0b1706a3fa9a4f96ea17e32
DSU-adbabcock <austin.babcock@trojans.dsu.edu>
2018-12-04 20:06:14 -0600

    fixing drawables folder
    before updating sdk

---
+++ dbfzv1/Adapter/ImageAdapter.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using Java.Lang;
+
+namespace dbfzv1.Adapter
+{
+    public class ImageAdapter : BaseAdapter
+    {
+        Context context;
+
+        public ImageAdapter (Context c)
+        {
+            context = c;
+        }
+
+        public override int Count
+        {
+            get { return /*thumbIds.Length;*/2; }
+        }
+
+        public override Java.Lang.Object GetItem (int position)
+        {
+            return null;
+        }
+
+        public override long GetItemId(int position)
+        {
+            return 0;
+        }
+
+        // create a new ImageView for each item referenced by the Adapter
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            ImageView imageView;
+
+            if (convertView == null)
+            {  // if it's not recycled, initialize some attributes
+                imageView = new ImageView(context);
+                imageView.LayoutParameters = new GridView.LayoutParams(85, 85);
+                imageView.SetScaleType(ImageView.ScaleType.CenterCrop);
+                imageView.SetPadding(8, 8, 8, 8);
+            }
+            else
+            {
+                imageView = (ImageView)convertView;
+            }
+
+            //imageView.SetImageResource(thumbIds[position]);
+            return imageView;
+        }
+
+        // references to our images
+        /*int[] thumbIds = {
+        Resource.Drawable.a, Resource.Drawable.sample_3,
+        Resource.Drawable.sample_4, Resource.Drawable.sample_5,
+        Resource.Drawable.sample_6, Resource.Drawable.sample_7,
+        Resource.Drawable.sample_0, Resource.Drawable.sample_1,
+        Resource.Drawable.sample_2, Resource.Drawable.sample_3,
+        Resource.Drawable.sample_4, Resource.Drawable.sample_5,
+        Resource.Drawable.sample_6, Resource.Drawable.sample_7,
+        Resource.Drawable.sample_0, Resource.Drawable.sample_1,
+        Resource.Drawable.sample_2, Resource.Drawable.sample_3,
+        Resource.Drawable.sample_4, Resource.Drawable.sample_5,
+        Resource.Drawable.sample_6, Resource.Drawable.sample_7
+    };*/
+    }
+}
+
+
--- dbfzv1/MainActivity.cs
+++ dbfzv1/MainActivity.cs
@@ -6,6 +6,8 @@ using Android.Widget;
 using System.Collections;
 using System.Collections.Generic;
 using dbfzv1.Adapter;
+using Android.Content;
+using Android.Views;
 
 namespace dbfzv1
 {
@@ -17,7 +19,10 @@ namespace dbfzv1
                                 "SSJ Goku", "Goku Blue", "Goku Black", "Gotenks", "Hit", "Kid Buu", "Krillin", "Majin Buu", "Nappa", "Piccolo", "Tien", "Trunks", "Base Vegeta", "SSJ Vegeta",
                                 "Vegeta Blue", "Vegito", "Yamcha", "Zamasu"};
 
-        List<string> lstResult = new List <string>();
+
+        
+        ArrayList charlist;
+        GridView gridView;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -25,16 +30,31 @@ namespace dbfzv1
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
 
-            GridView gridView = FindViewById<GridView>(Resource.Id.gridView);
-            GridViewAdapter gridViewAdapter = new GridViewAdapter(lstResult, this);
-            gridView.Adapter = gridViewAdapter;
-            SetupList();
+            gridView = FindViewById<GridView>(Resource.Id.gridView);
+            getCharList();
+            gridView.Adapter = new ImageAdapter(this);
+
+            //BIND
+            //gridView.Adapter = adapter;
+
+            gridView.ItemClick += gridView_ItemClick;
+
+
         }
 
-        private void SetupList()
+        //FILL DATA
+        private void getCharList()
         {
+            charlist = new ArrayList();
             foreach (var item in characters)
-                lstResult.Add(item);
+                charlist.Add(item);
+
+        }
+
+        void gridView_ItemClick(object sender, AdapterView.ItemClickEventArgs e)
+        {
+            StartActivity(typeof(Activity1));
         }
     }
+
 }
