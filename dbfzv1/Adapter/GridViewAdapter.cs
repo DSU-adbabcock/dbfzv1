45eda021270f8507b44781545344b7778c6f8f36
DSU-adbabcock <austin.babcock@trojans.dsu.edu>
2018-12-04 18:12:21 -0600

    Add project files.

---
+++ dbfzv1/Adapter/GridViewAdapter.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using Java.Lang;
+
+namespace dbfzv1.Adapter
+{
+    class GridViewAdapter : BaseAdapter
+    {
+
+        List<string> lstSource;
+        Context context;
+
+        public GridViewAdapter(List<string> lstSource,Context context)
+        {
+            this.lstSource = lstSource;
+            this.context = context;
+        }
+        public override int Count
+        {
+            get
+            {
+                return lstSource.Count;
+            }
+        }
+
+        public ComplexUnitType Sp { get; private set; }
+
+        public override Java.Lang.Object GetItem(int position)
+        {
+            return position;
+        }
+
+        public override long GetItemId(int position)
+        {
+            return position;
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            Button button = null;
+            if (convertView == null)
+            {
+                button = new Button(context);
+                button.LayoutParameters = new GridView.LayoutParams(WindowManagerLayoutParams.WrapContent, WindowManagerLayoutParams.WrapContent);
+                button.SetPadding(8, 8, 8, 8);
+                button.SetBackgroundColor(Color.DarkGray);
+                button.SetTextColor(Color.Yellow);
+                button.SetTextSize(Sp, 30);
+                button.Text = lstSource[position]; //wut da hell
+                button.Click += delegate
+                {
+                    Toast.MakeText(context, "" + button.Text, ToastLength.Short).Show();
+                };
+            }
+            else
+                button = (Button)convertView;
+            return button;
+
+        }
+    }
+}
---
+++ dbfzv1/MainActivity.cs
@@ -0,0 +1,40 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System.Collections;
+using System.Collections.Generic;
+using dbfzv1.Adapter;
+
+namespace dbfzv1
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/Theme.AppCompat.Light.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+
+        string[] characters = {"Android 16", "Android 17", "Android 18", "Android 21", "Bardock", "Beerus", "Broly", "Ginyu", "Cell", "Cooler", "Frieza", "Teen Gohan", "Adult Gohan", "Base Goku",
+                                "SSJ Goku", "Goku Blue", "Goku Black", "Gotenks", "Hit", "Kid Buu", "Krillin", "Majin Buu", "Nappa", "Piccolo", "Tien", "Trunks", "Base Vegeta", "SSJ Vegeta",
+                                "Vegeta Blue", "Vegito", "Yamcha", "Zamasu"};
+
+        List<string> lstResult = new List <string>();
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            GridView gridView = FindViewById<GridView>(Resource.Id.gridView);
+            GridViewAdapter gridViewAdapter = new GridViewAdapter(lstResult, this);
+            gridView.Adapter = gridViewAdapter;
+            SetupList();
+        }
+
+        private void SetupList()
+        {
+            foreach (var item in characters)
+                lstResult.Add(item);
+        }
+    }
+}
